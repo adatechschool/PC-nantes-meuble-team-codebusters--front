6139991597b1f6778b0c730b1242031205245d68
LaurieBiguet <biguet.laurie@live.fr>
2023-03-16 10:30:51 +0100

    userButton

--- codebusters/src/App.js
+++ codebusters/src/App.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Gallery from './components/Gallery';
+import UserButton from './components/UserButton'
 
 
 
 export default function App() {
   return (
     <div className="App">
-      <IconFavorite />
+      <UserButton />
     </div>
   );
 }
