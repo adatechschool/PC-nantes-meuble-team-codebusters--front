66dc7f38b2ea2adc959e033644525ecae9dec44f
Corentin MONVILLERS <c.monvillers@live.fr>
2023-03-20 12:28:35 +0100

    productForm

--- codebusters/src/pages/ProductForm.js
+++ codebusters/src/pages/ProductForm.js
@@ -1,15 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 import Navbarblack from "../components/TestComponents/Navbarblack";
 import ProductCard from "../components/ProductCard";
 
 export default function ProductForm() {
-    return (
-        <div className="App" style={{ backgroundColor: "#FBF4F4" }}>
+  return (
+    
+      <div className="App" style={{ backgroundColor: "#FBF4F4" }}>
         <Navbarblack />
-        <ProductCard />
-        <h1>Product Form PAGE</h1>
-        <div className='Card'>
+        <div className="Card">
+          <ProductCard />
         </div>
-    </div>
-    );
-  }
+      </div> 
+  );
+}
