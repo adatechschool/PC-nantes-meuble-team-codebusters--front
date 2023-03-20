4c490e32e6f9f79a69d49f9ee07c51541926e8dd
Lorena <lorenacamposrosas@gmail.com>
2023-03-20 17:00:40 +0100

    add divs for forms

--- codebusters/src/pages/Login.js
+++ codebusters/src/pages/Login.js
@@ -4,7 +4,25 @@ export default function Home() {
     return (
         <div>
             <h1>LOGIN PAGE</h1>
-        
-      </div>
+            <div class="login">
+            <form onSubmit=''>
+                <label>
+                Name:
+                <input type="text" value='' onChange='' />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            </div>
+            <div class="registration">
+            <form onSubmit=''>
+                <label>
+                Name:
+                <input type="text" value='' onChange='' />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            </div>
+        </div>
     );
-  }
+}
+
