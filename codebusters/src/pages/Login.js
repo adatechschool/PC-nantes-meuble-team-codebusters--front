73c4cf72bb4d91474098cb341698d502d8dacb5a
Corentin MONVILLERS <c.monvillers@live.fr>
2023-03-20 10:25:18 +0100

    navbarblack

--- codebusters/src/pages/Login.js
+++ codebusters/src/pages/Login.js
@@ -1,7 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import Navbarblack from "../components/TestComponents/Navbarblack";
 
 export default function Home() {
     return (
+        <div className="App" style={{ backgroundColor: "#FBF4F4" }}>
+        <Navbarblack />
         <h1>LOGIN PAGE</h1>
+    </div>
     );
   }
