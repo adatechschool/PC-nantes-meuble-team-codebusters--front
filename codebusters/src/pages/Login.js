3809a48ae65da4a20faebf25d56fe9cfc9871ec2
Juliettedqb <juliettedqb@gmail.com>
2023-03-20 21:41:13 +0100

    add ApiController

--- codebusters/src/pages/Login.js
+++ codebusters/src/pages/Login.js
@@ -1,11 +1,59 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect, useState } from "react";
 import Navbarblack from "../components/TestComponents/Navbarblack";
+import {
+  callLogin,
+  createFurniture,
+  findUserFurnitures,
+} from "../data/ApiController";
 
 export default function Login() {
-    return (
-        <div className="App" style={{ backgroundColor: "#FBF4F4" }}>
-        <Navbarblack />
-        <h1>LOGIN PAGE</h1>
-    </div>
-    );
+  const [userFurniture, setUserFurniture] = useState([]);
+
+  useEffect(() => {
+    findUserFurnitures().then((res) => {
+      setUserFurniture(res);
+    });
+  }, []);
+
+  async function login() {
+    const response = await callLogin("emilie@gmail.com", "1234567");
+    console.log(response);
   }
+
+  async function getUserFurnitures() {
+    const response = await findUserFurnitures();
+    console.log(response);
+    setUserFurniture(response);
+  }
+
+  async function onCreateFurniture() {
+    const response = await createFurniture(
+      new Date(),
+      "assise",
+      "pouf",
+      120,
+      [
+        {
+          url: "https://prodimages.casashops-devcdn.com/productimages/ambiant/664965/DiSbVEWy4hZ83eiHJuHT30.jpg?quality=75&width=667&fit=contain",
+          view: "front",
+        },
+      ],
+      "ceci est un pouf encore",
+      true
+    );
+    console.log(response);
+  }
+  return (
+    <div className="App" style={{ backgroundColor: "#FBF4F4" }}>
+      <Navbarblack />
+      {userFurniture.map((item) => (
+        <p>{item.description[0]}</p>
+      ))}
+      <h1>LOGIN PAGE</h1>
+      <button onClick={login}>click</button>
+      <button onClick={getUserFurnitures}>click</button>
+      <button onClick={onCreateFurniture}>click</button>
+    </div>
+  );
+}
