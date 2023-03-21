92362fa17c3f9c83a0ae3464b4dafe48632a88d6
Theo Seuge <theoseuge@neey.local>
2023-03-21 14:26:35 +0100

    fix admin page + add button true false

--- codebusters/src/App.js
+++ codebusters/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from './components/footer/Footer';
-import Cards from './components/Cards'
-import Headerwhite from './components/HeaderWhite';
 
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import ProductForm from './pages/ProductForm';
+import Admin from './pages/Admin';
 
 
 export default function App() {
@@ -17,6 +16,7 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/product/:id" element={<ProductForm />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
         <Footer />
     </div>
