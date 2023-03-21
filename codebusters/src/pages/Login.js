63a7e790052ded2af9abb03ed5b0bca702212225
Lorena <lorenacamposrosas@gmail.com>
2023-03-21 12:31:13 +0100

    add registration and login forms

--- codebusters/src/pages/Login.js
+++ codebusters/src/pages/Login.js
@@ -2,44 +2,85 @@ import "bootstrap/dist/css/bootstrap.css";
 import Navbarblack from "../components/TestComponents/Navbarblack";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import "../App.css"
 
 const formStyle = {
-  width: "20%",
+  display: "flex",
+  width: "20em",
   margin: "auto",
+  flexDirection:"column"
 };
+const title ={
+ fontSize:"1.5em",
+ marginBottom:"1em",
+ textAlign: "center"
+}
 
 const wrapperForm = {
-  paddingTop: "15em",
-  marginBottom: "4em"
+  display: "Flex",
+  justifyContent : "space-evenly",
+  paddingTop: "10em",
+  marginBottom: "4em",
+  textAlign: "left"
 };
+
+const buttonStyle = {
+   border: "rgba(154, 113, 85, 0.2)",
+  backgroundColor: "rgba(175, 128, 96, 0.928)",
+  alignSelf: "center"
+}
 
 export default function Login() {
   return (
     <div style={{ backgroundColor: "#FBF4F4" }}>
       <Navbarblack />
       <div style={wrapperForm}>
-        
-        <Form style={formStyle}>
-          <h1>Login !</h1>
-          <Form.Group className="mb-2" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+        <div className="loginForm" >
+          <Form style={formStyle}>
+            <h1 style={title}>Connectez-vous</h1>
+            <Form.Group className="mb-2" controlId="formBasicEmail">
+              <Form.Label>Adresse email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                Nous ne partagerons jamais votre adresse email.
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+            <Button style={buttonStyle} variant="primary" type="submit">
+              Envoyez
+            </Button>
+          </Form>
+        </div>
+        <div className="registrationForm" >
+          <Form style={formStyle}>
+            <h1 style={title}>Enregistrez-vous</h1>
+            <Form.Group className="mb-2" controlId="formBasicName">
+              <Form.Label>Nom</Form.Label>
+              <Form.Control type="name" placeholder="Enter name" />
+            </Form.Group>
+            <Form.Group className="mb-2" controlId="formBasicEmail">
+              <Form.Label>Adresse email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                Nous ne partagerons jamais votre adresse email.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Mot de passe</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+  
+            <Button style={buttonStyle} variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     </div>
   );
