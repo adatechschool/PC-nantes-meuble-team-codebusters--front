847ca279befb4f3537b933f6eb0061d79c2e7e69
LaurieBiguet <biguet.laurie@live.fr>
2023-03-23 14:12:27 +0100

    profil

--- codebusters/src/pages/User.js
+++ codebusters/src/pages/User.js
@@ -5,11 +5,37 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {createFurniture} from "../data/ApiController";
 
+
+const imgSize = {
+  width: "10rem",
+  height: "10rem",
+  objectFit: "cover",
+  borderRadius: "50%"
+};
+const wrapperForm = {
+  display: "Flex",
+  marginTop: "8em",
+  marginBottom: "10em",
+  textAlign: "left",
+};
+
 const buttonStyle = {
-    border: "rgba(154, 113, 85, 0.2)",
-   backgroundColor: "rgba(175, 128, 96, 0.928)",
-   alignSelf: "center"
+  border: "rgba(154, 113, 85, 0.2)",
+  backgroundColor: "rgba(175, 128, 96, 0.928)",
+  alignSelf: "center"
  }
+
+const infosUser = {
+  display: "flex",
+  paddingTop: "9em",
+  textAlign: "left",
+}
+
+const furniture =  {
+  paddingTop: "9em",
+  marginRight: "3em"
+}
+
 
  async function newFurniture(event) {
     event.preventDefault();
@@ -25,10 +51,23 @@ const buttonStyle = {
 
 export default function PostNewFurniture() {
   return (
-    <div style={{ backgroundColor: "#FBF4F4", marginTop: "70px" }}>
+    <div style={{ backgroundColor: "#FBF4F4"}}>
       <Navbarblack />
-    <div>
-    <Form className='col-3' onSubmit={newFurniture}>
+
+    <div class='container'>
+      <div class='row'>
+    
+      <div class="col-3">
+      <div style={infosUser}>
+        <div>
+        <div>Bonjour Emilie !</div>
+        <div>Email : emilie.gmail.com</div>
+        </div>
+        </div>
+
+
+    <div style={wrapperForm}>
+    <Form onSubmit={newFurniture}>
       
       <Form.Group className="mb-3" controlId="formBasicCategory">
         <Form.Label>Catégorie</Form.Label>
@@ -37,7 +76,7 @@ export default function PostNewFurniture() {
 
       <Form.Group className="mb-3" controlId="formBasicImage">
         <Form.Label>Type</Form.Label>
-        <Form.Control type="text" placeholder="Entrez l'url de votre image" id="inputTypeNewFurniture"/>
+        <Form.Control type="text" placeholder="Entrez le nom de votre meuble" id="inputTypeNewFurniture"/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPrice">
@@ -47,7 +86,7 @@ export default function PostNewFurniture() {
 
       <Form.Group className="mb-3">
         <Form.Label>Photos</Form.Label>
-        <Form.Control type="text" placeholder="Ex : Largeur 50 cm, Hauteur 80cm, Profondeur 40cm" id="inputPhotosNewFurniture"/>
+        <Form.Control type="text" placeholder="Entrez l'url de votre image" id="inputPhotosNewFurniture"/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicDescrption">
@@ -59,6 +98,28 @@ export default function PostNewFurniture() {
         Envoyer
       </Button>
     </Form>
+    </div>
+    </div>
+    
+    <div class="col" style={furniture}>
+      <table className="table">
+        <thead>
+            <tr>
+              <th scope="col">Photos</th>
+              <th scope="col">Category</th>
+              <th scope="col">Type</th>
+              <th scope="col">Description</th>
+              <th scope="col">Price</th>
+              <th scope="col">Availability</th>
+              <th scope="col">Delete / Modify</th>
+            </tr>
+        </thead>
+        
+
+      </table>
+      </div>
+
+    </div>
     </div>
     </div>
   );
